cafa5f6943bb46b328b0fa566df3c156fe37d6c9
Rakib <mdrakibul.cse@gmail.com>
2019-05-17 08:47:22 +0600

    Add seats availability in condition

--- buy_ticket.py
+++ buy_ticket.py
@@ -5,7 +5,7 @@ import time
 
 
 url = 'https://www.shohoz.com/booking/bus/search/results/0/NzYxNQ=='
-cookie = 'gIS_SoyCs-vYtBe8D--5lGE0ZjAzMWVlMWE3MjA5OTcSLv1=1558034986778; FJuWOYPHRbh49l8qXPG6pGY3ZGNjYmU4YWUxNTk1YmESLv1=1558038094329; _ga=GA1.2.345490640.1558030320; _gid=GA1.2.787299828.1558030320; gIS_SoyCs-vYtBe8D--5lGE0ZjAzMWVlMWE3MjA5OTcSLv1=1558030321035; WZRK_G=aa487ef1d2ce434a8be8d72570816933; __tawkuuid=e::shohoz.com::Fl1gmGGstPO6bBbFYZc4ucIKmWxdnhKqTDzubHUZbfvvPRFKNSYr3e4ySDCrEPtg::2; _gat=1; TawkConnectionTime=0; wwwu=eyJpdiI6IlRvTm5HcHpEWEp3T1graCtZV2wyU0xjZnBqY3RTRTU1eXZiVnhRWjRneFE9IiwidmFsdWUiOiIzYitGUmVnVDJZTmlTMEVnbG5tNFdmZjFha3hjK015d0J5MWRiU3o3dk5ua1BjZXRlMEpwejVMRnhZK0thNm9GeEJ0cm1jenJ5SVZvZXFlYzhGSWJaUT09IiwibWFjIjoiMGNjNDQ4NWVkMWJlYjM2ZjZiYWMxMjU4YjUxNmNlNjgxYzI3NmIwMDAxMzY2ODc2MTU0MjcwNGY1N2YyNTliOSJ9; laravel_session=eyJpdiI6IjFqeHJ5RlwveTMwMWlrc1RFMVg4bVB6dHdRa2FKN0dDTXJ0MFpGbWdcLzJKQT0iLCJ2YWx1ZSI6IlJ4YkJ6d0NPMTlYUENzclBIMmhpZ3BcL3FMd0pKbzY0Q0Y3NHRYdWsrSmJEd3BZNW5EajhGK2NBTDRxb0c3bE9jMU41T3E4RlJxNGJJdHNWMHlQWDFSZz09IiwibWFjIjoiYTkyYWFkYjc5NTNkMDRjYmQwZDFmOGExMmZhNTgxMWJlNjdlMTUwODNkZWZkMjM2YzUyMzdiMDQ1ZWNhYTcyMyJ9; WZRK_S_485-K65-RR5Z=%7B%22p%22%3A2%2C%22s%22%3A1558038089%2C%22t%22%3A1558038094%7D'
+cookie = 'gIS_SoyCs-vYtBe8D--5lGE0ZjAzMWVlMWE3MjA5OTcSLv1=1558060279904; FJuWOYPHRbh49l8qXPG6pGY3ZGNjYmU4YWUxNTk1YmESLv1=1558060280467; _ga=GA1.2.345490640.1558030320; _gid=GA1.2.787299828.1558030320; gIS_SoyCs-vYtBe8D--5lGE0ZjAzMWVlMWE3MjA5OTcSLv1=1558030321035; WZRK_G=aa487ef1d2ce434a8be8d72570816933; __tawkuuid=e::shohoz.com::Fl1gmGGstPO6bBbFYZc4ucIKmWxdnhKqTDzubHUZbfvvPRFKNSYr3e4ySDCrEPtg::2; TawkConnectionTime=0; wwwu=eyJpdiI6IkpEeXlvU3I2eGUyY3U1c3lnMVZjU2VQZmdiSWpuTHp3RVRWeVdFaWZNMnM9IiwidmFsdWUiOiJlRVo2U21YVGZIQ0RVN2VwS1ZCN3lVQXVycGhmVm00b1JiYnBWNCtNTVl1VzRhN0ZjNDVOVzRkNmQrVUdTbmU3RlVFb2pcL2licTUxR3dsZ3NwWnNEN2c9PSIsIm1hYyI6ImY2MTc3MTIzMDk5MTI4NGVkMzIzZDkxYmYwN2Y5MTg5OTkyNzg2ODg0NmI2ZDU3MThmMDAzMzU0OTM0MzhhNDcifQ%3D%3D; laravel_session=eyJpdiI6ImVyalZrVFRIN0NtSjBUYUozblBJaEhyY3hFWjAwaUs4WkF2amJRd0xsdzQ9IiwidmFsdWUiOiJpOEI0Q2JUb1UrMElXK3ZpRks2OHFUbmFiT2dCZnVWbkNGTktRNUdvRjIxSXVORHRUNHFkbmk2S2hiQzhsWmdsU2hIZlRhK2ZTZVh2UnRWdDZLK1RUdz09IiwibWFjIjoiNWJhZGJmNDk0NjljZjZmNWU2MGYwMWIwOWQwN2VjNjkyZjNkMTI0NzJkYzU0ZGFmNDY4ZjJiM2VjOWIxMDIxNiJ9; WZRK_S_485-K65-RR5Z=%7B%22p%22%3A4%2C%22s%22%3A1558060227%2C%22t%22%3A1558060280%7D'
 const_op_name = ['Nabil', 'S.R', 'Hanif', 'Shyamoli']
 const_bus_type = ['Hyundai', 'Scania', 'VOLVO']
 
@@ -21,8 +21,9 @@ def find_ticket():
                 operator_details = columns[0].find('ul').find_all('li')
                 op_name = operator_details[0].text.split()
                 bus_type = operator_details[1].text.split()
+                seats_available = int(columns[3].text)
 
-                if any({*const_op_name} & {*op_name}) and any({*const_bus_type} & {*bus_type}):
+                if any({*const_op_name} & {*op_name}) and any({*const_bus_type} & {*bus_type}) and seats_available > 0:
                     return True
             return False
         else:
